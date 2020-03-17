f0cb84d125f92aae2a7cce2358b685f18c297f0b
jfmull <james.f.mullin@gmail.com>
2020-03-17 10:24:12 -0400

    updated

--- identity.m
+++ identity.m
@@ -3,7 +3,7 @@ function X = identity(X)
 %   In mathematics, an identity function, also called an identity relation or identity map or
 %   identity transformation, is a function that always returns the same value that was used as its
 %   argument. In equations, the function is given by f(x) = x.
-%
+%  added comment from a new release. check if rake task gets it
 %   https://en.wikipedia.org/wiki/Identity_function
 
     X;

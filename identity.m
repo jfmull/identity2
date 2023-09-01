eb723121da74be466ff23d8eaab74f89693e7abc
jfmull <james.f.mullin@gmail.com>
2023-09-01 13:44:56 -0400

    Update identity.m

--- identity.m
+++ identity.m
@@ -6,6 +6,7 @@ function Y = identity(X)
 %  added comment from a new release. check if rake task gets it
 %   https://en.wikipedia.org/wiki/Identity_function
 % change from 23b pre-release
+% test comment for bash
 
     Y = X;
 end
